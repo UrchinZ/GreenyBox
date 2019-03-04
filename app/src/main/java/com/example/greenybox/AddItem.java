453389f1ccf0e7585a500e34e451ae66ac6e8882
Huanzhi Zhu <justicepigzhz@gmail.com>
2019-03-04 18:21:15 -0500

    Add select from album function

--- app/src/main/java/com/example/greenybox/AddItem.java
+++ app/src/main/java/com/example/greenybox/AddItem.java
@@ -1,20 +1,36 @@
 package com.example.greenybox;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.DatePickerDialog;
+import android.content.ContentUris;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -33,7 +49,11 @@ public class AddItem extends AppCompatActivity {
     private  DatePickerDialog.OnDateSetListener eDateSetListener; //expiration date event listener
 
     private EditText pDays; //days to preserve
-    private EditText itemName;//Item Name
+    private EditText itemName; //Item Name
+
+    private final String TAG = getClass().getSimpleName();
+    private ImageView mPicture;
+    private static final int CHOOSE_PHOTO = 385;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -184,6 +204,19 @@ public class AddItem extends AppCompatActivity {
             }
         };
 
+
+        // Click to select image from album
+        mPicture = findViewById(R.id.ivPicture);
+        ImageView mChooseFromAlbum = findViewById(R.id.ivPicture);
+
+
+        mChooseFromAlbum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAlbum();
+            }
+        });
+
     }
 
     /**
@@ -219,5 +252,127 @@ public class AddItem extends AppCompatActivity {
             e.printStackTrace();
         }
         return 0;
+    }
+
+
+    /**
+     * Create Intent to open imageView.
+     */
+    private void openAlbum() {
+        Intent openAlbumIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        openAlbumIntent.setType("image/*");
+        startActivityForResult(openAlbumIntent, CHOOSE_PHOTO);
+    }
+
+    /**
+     * Get Permission Request Results.
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "onRequestPermissionsResult: permission granted");
+        } else {
+            Log.i(TAG, "onRequestPermissionsResult: permission denied");
+            Toast.makeText(this, "You Denied Permission", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Get Image Activity data.
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case CHOOSE_PHOTO:
+                if (data == null) {
+                    return;
+                }
+                Log.i(TAG, "onActivityResult: ImageUriFromAlbum: " + data.getData());
+                if (resultCode == RESULT_OK) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                        handleImageOnKitKat(data);
+                    } else {
+                        handleImageBeforeKitKat(data);
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * Get data image path. (Standard)
+     * @param data
+     */
+    private void handleImageBeforeKitKat(Intent data) {
+        Uri uri = data.getData();
+        String imagePath = getImagePath(uri, null);
+        displayImage(imagePath);
+    }
+
+    /**
+     * Get data image path. (version compatible)
+     * @param data
+     */
+    @TargetApi(19)
+    private void handleImageOnKitKat(Intent data) {
+        String imagePath = null;
+        Uri uri = data.getData();
+        if (DocumentsContract.isDocumentUri(this, uri)) {
+            String docId = DocumentsContract.getDocumentId(uri);
+            if ("com.android.providers.media.documents".equals(uri.getAuthority())) {
+                String id = docId.split(":")[1];
+                String selection = MediaStore.Images.Media._ID + "=" + id;
+                imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection);
+            } else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())) {
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(docId));
+                imagePath = getImagePath(contentUri, null);
+            }
+        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            imagePath = getImagePath(uri, null);
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            imagePath = uri.getPath();
+        }
+        displayImage(imagePath);
+    }
+
+    /**
+     * Display Image.
+     * @param imagePath
+     */
+    private void displayImage(String imagePath) {
+        if (imagePath != null) {
+            Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+            mPicture.setImageBitmap(bitmap);
+        } else {
+            Toast.makeText(this, "failed to get image", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    /**
+     *
+     * @param uri
+     * @param selection
+     * @return
+     */
+    private String getImagePath(Uri uri, String selection) {
+        String path = null;
+        Cursor cursor = getContentResolver().query(uri, null, selection, null, null);
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+        return path;
     }
 }
