ddda9094ee3c79d778a55eaf3a21ba0ab7aac6cc
Judy Z <2j.zy.zhang@gmail.com>
2019-03-05 13:29:16 -0500

    Item Class
    Changed Brendan's foodItem class

---
+++ app/src/main/java/com/example/greenybox/Item.java
@@ -0,0 +1,70 @@
+package com.example.greenybox;
+
+import java.util.Calendar;
+
+//https://stackoverflow.com/questions/40124189/android-difference-between-two-dates
+public class Item {
+    String name;
+    Calendar buyDate;
+    Calendar expDate;
+    int storeDay;
+
+    //default constructor
+    public Item(){
+        name = "";
+        storeDay = -1;
+    }
+
+    //constructor from storage string
+    public Item(String s){
+
+    }
+
+    //modifier
+    public void setName(String n){
+        name = n;
+    }
+
+    public void setBuyDate(int year, int month, int day){
+        /*somehow magically parse year, month, day into a calendar object*/
+        pass;
+    }
+
+    public void setExpDate(int year, int month, int day){
+        /*somehow magically parse year, month, day into a calendar object*/
+        pass;
+    }
+
+    public void setStoreDay(int day){
+        storeDay = day;
+    }
+
+    //accessor
+    public String getName(){
+        return name;
+    }
+
+    public  Calendar getBuyDate(){
+        return buyDate;
+    }
+
+    public Calendar getExpDate(){
+        return expDate;
+    }
+
+    public int getStoreDay(){
+        return storeDay;
+    }
+
+    /*helper function*/
+    private int dayDifference(){
+        /*calculate difference between 2 dates*/
+        return 0;
+    }
+
+    private void makeConsistent(){
+        /*resolve conflict between all variables*/
+    }
+
+
+}
