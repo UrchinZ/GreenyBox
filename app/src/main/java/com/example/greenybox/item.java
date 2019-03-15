8d254ce7f2002cecec701a41f17a7355322904f6
mcfadb <mcfadb@rpi.edu>
2019-03-15 16:34:40 -0400

    Item with stuff from master branch

--- app/src/main/java/com/example/greenybox/item.java
+++ app/src/main/java/com/example/greenybox/item.java
@@ -2,7 +2,7 @@ package com.example.greenybox;
 
 import org.joda.time.LocalDate;
 
-public class item {
+public class Item {
     //Data
     private LocalDate expDate;
     private LocalDate buyDate;
@@ -11,11 +11,11 @@ public class item {
     private int count;
     //Constructor
     //Takes in 5 values for the data and assigns them.
-    public item(String namu, LocalDate EXP, LocalDate PerDate, int rem, int coun){
+    public Item(String namu, LocalDate EXP, LocalDate PerDate, int rem, int coun){
         name=namu; expDate=EXP; buyDate =PerDate; reminder =rem; count=coun;
     }
     //A constructor for if you have an amount of days until experation.
-    public item(String newName, int EXPlus, LocalDate perDate, int rem, int coun){
+    public Item(String newName, int EXPlus, LocalDate perDate, int rem, int coun){
         name=newName; expDate=perDate.plusDays(EXPlus); buyDate =perDate; reminder =rem; count=coun;
     }
     //Helper functions
@@ -23,25 +23,15 @@ public class item {
     public LocalDate remDate(){return expDate.minusDays(reminder);}
     // Observers
     // Returns: number of days from reminder to experation
-    public int getReminder() {
-        return reminder;
-    }
+    public int getReminder() {return reminder;}
     //Returns: Localdate Purchase date
-    public LocalDate getBuyDate() {
-        return buyDate;
-    }
+    public LocalDate getBuyDate() {return buyDate;}
     //Returns: Localdate of expiration
-    public LocalDate getexpDate() {
-        return expDate;
-    }
+    public LocalDate getExpDate() {return expDate;}
     //Returns: name as string
-    public String getName() {
-        return name;
-    }
+    public String getName() {return name;}
     //Returns: amount of item
-    public int getCount(){
-        return count;
-    }
+    public int getCount(){return count;}
     //Notify
 
     //Modifier
