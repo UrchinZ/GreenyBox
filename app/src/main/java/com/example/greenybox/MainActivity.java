3ef9c2f0d31e347108368f9937874c21f6ac0c53
Riantix <Riantix@Ridth.localdomain>
2019-04-11 19:10:25 -0400

    Added freshness button. Name. Still a problem. Will be fixed.

--- app/src/main/java/com/example/greenybox/MainActivity.java
+++ app/src/main/java/com/example/greenybox/MainActivity.java
@@ -249,4 +249,30 @@ public class MainActivity extends AppCompatActivity {
 
         grid_photo.setAdapter(mAdapter);
     }
+
+    /**
+     *
+     *
+     */
+    public void sortFreshness(View view) {
+        Collections.sort(mData, new Comparator<Item>() {
+            @Override
+            public int compare(Item o1, Item o2) {
+                if (o1.Freshness() > o2.Freshness()) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            }
+        });
+        GridView grid_photo = (GridView) findViewById(R.id.grid_photo);
+        final BaseAdapter mAdapter = new MyAdapter<Item>(mData, R.layout.item_grid) {
+            @Override
+            public void bindView(ViewHolder holder, Item obj) {
+                holder.setText(R.id.txt_icon, obj.getName());
+            }
+        };
+
+        grid_photo.setAdapter(mAdapter);
+    }
 }
